6ebc83fa4526dd98c31ae03bcc2c4d145e0e8484
Sarwar <sarwar.cse.bu@gmail.com>
2019-09-22 00:25:07 +0600

    template update

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('master')
+@section('contents')
+<link rel="stylesheet" type="text/css" href="{{asset('css/index.css')}}">
+<div class="imageintro">
+	<img src="{{asset('images/cover.png')}}">
+	
+</div>
+@endsection
