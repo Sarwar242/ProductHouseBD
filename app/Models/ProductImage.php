6ae0c07e3f9c4a7619cdd4f114117b4123596b89
Sarwar <sarwar.cse.bu@gmail.com>
2019-09-24 23:57:27 +0600

    test1

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -4,10 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class ProductImage extends Model
 {
     public function productImage()
     {
-        $this->hasOne(Product::class);
+        $this->belongsTo(Product::class);
     }
 }
