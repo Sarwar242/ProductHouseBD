7e14fc422f61a1c0ae853941f8e77bdc471c5aff
Sarwar <sarwar.cse.bu@gmail.com>
2019-12-07 01:13:04 +0600

    ca

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->tinyInteger('gender')->comment('0-Female|1-Male');
-
             $table->string('ip_address')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
