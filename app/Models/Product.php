d37bff8754b7b743965cc4f9884ff4180c7bc638
Sarwar <sarwar.cse4.bu@gmail.com>
2020-07-02 14:03:00 +0600

    Algolia Removed!

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,31 +1,17 @@
 <?php
 
 namespace App\Models;
-use Laravel\Scout\Searchable;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use Searchable;
+
     protected $guarded = [];
 
     protected $fillable = [
         'product_name', 'product_code', 'product_price', 'product_details',
         'product_discount', 'product_quantity',
     ];
-
-    public function searchableAs()
-    {
-        return 'product_name';
-    }
-    public function toSearchableArray()
-    {
-        $array = $this->toArray();
-
-
-
-        return $array;
-    }
 
     
     public function productImages()
